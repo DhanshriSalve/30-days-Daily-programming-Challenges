29c8d80038b5f1ea57f5878c77b02f4c0feb8234
Dhanshri Salve <155350468+DhanshriSalve@users.noreply.github.com>
2024-09-10 23:42:48 +0530

    Add files via upload

---
+++ practice question-2.py
@@ -0,0 +1,60 @@
+# You are given an unsorted array of integers. Your task is to find the median of the array.
+# The median is the middle value in an ordered list of numbers. If the list has an even number of elements,
+# the median is 100 the average of the two middle numbers.
+
+
+
+# Implement a function that returns the median of the array without explicitly sorting the entire array.
+
+# Example 1:
+# arr = [3, 2, 1, 4, 5]
+# Output: 3
+
+# Example 2:
+# arr = [7, 10, 4, 3, 20, 15]
+# Output: 8.5
+
+# import random
+# def sorted(arr,k):
+#     if len(arr)==1:
+#         return arr[0]
+    
+#     p=random.choice(arr)
+#     lows=[ele for ele in arr if <p]
+#     high=[ele for ele in arr if > p]
+#     pivots=[ele for ele in ar ele == p]
+    
+#     if k < len(lows):
+        
+import random
+
+def sortarr(arr, k):
+    if len(arr) == 1:
+        return arr[0]
+    
+    pivot = random.choice(arr)
+    smallno= [el for el in arr if el < pivot]
+    greater= [el for el in arr if el > pivot]
+    equal= [el for el in arr if el == pivot]
+    
+    if k < len(smallno):
+        return sortarr(smallno, k)
+    elif k < len(smallno) + len(equal):
+        return equal[0]
+    else:
+        return sortarr(greater, k - len(smallno) - len(equal))
+
+def find_median(arr):
+    n = len(arr)
+    if n % 2 == 1:
+        return sortarr(arr, n // 2)
+    else:
+        return (sortarr(arr, n // 2 - 1) + sortarr(arr, n // 2)) / 2
+    
+#example 1    
+arr1 = [3, 2, 1, 4, 5]
+print(find_median(arr1))  
+
+#example 2
+arr2 = [7, 10, 4, 3, 20, 15]
+print(find_median(arr2))          
